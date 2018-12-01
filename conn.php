a030aaf201d655f6317af65d575284998ff57ea9
dharam-jakhar <36055801+dharam-jakhar@users.noreply.github.com>
2018-12-01 11:48:27 -0500

    removed credentials

--- conn.php
+++ conn.php
@@ -1,8 +1,8 @@
 <?php
-$servername = 'aa1lfewpz57tgbp.cxyxhcvs48cj.us-east-2.rds.amazonaws.com';
-$user = 'masterDB';
-$pass = 'yashika0504';
-$dbname = 'ebdb';
+$servername = '######';
+$user = '######';
+$pass = '######';
+$dbname = '######';
 $conn = new mysqli($servername, $user, $pass, $dbname) or die("Could not connect: ");
 $rs = @mysqli_select_db($conn, $dbname) or die("Sorry - cannot find the database"); 
 if(mysqli_connect_errno($conn))
